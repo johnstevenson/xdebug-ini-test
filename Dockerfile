369cb68833c4f3d0fdcd6bf0e16d3fa74ecdd337
johnstevenson <john-stevenson@blueyonder.co.uk>
2018-01-06 17:03:59 +0000

    Use docker to build custom php

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM johnstevenson/php-cli-exts
+
+WORKDIR /app
+ADD . /app
---
+++ tests.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo
+echo Simple Test
+echo -----------
+echo
+
+COUNTER=0
+while [  $COUNTER -lt 3 ]; do
+    php SimpleTests.php
+    let COUNTER=COUNTER+1
+    echo
+done
+
+echo
+echo Simple Test Merge
+echo -----------------
+echo
+
+COUNTER=0
+while [  $COUNTER -lt 3 ]; do
+    php SimpleTests.php --merge-inis
+    let COUNTER=COUNTER+1
+    echo
+done
+
+export COMPOSER_ALLOW_SUPERUSER=1
+echo
+echo Composer Test
+echo -------------
+echo
+
+COUNTER=0
+while [  $COUNTER -lt 3 ]; do
+    php composer-xdebug.phar --version --verbose
+    let COUNTER=COUNTER+1
+    echo
+done
+
+echo
+echo Composer Test Merge
+echo -------------------
+echo
+
+COUNTER=0
+while [  $COUNTER -lt 3 ]; do
+    php composer-xdebug.phar --version --verbose --merge-inis
+    let COUNTER=COUNTER+1
+    echo
+done
